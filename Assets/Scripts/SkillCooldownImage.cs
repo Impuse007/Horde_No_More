dad64d11ab1525bc19560fd74ccdd6ce4d1b119d
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2024-12-04 11:44:47 -0400

    Fixed the Cooldown Pictures
    The cooldown picture gameobject are now active if the skill is unlock

--- Assets/Scripts/SkillCooldownImage.cs
+++ Assets/Scripts/SkillCooldownImage.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class SkillCooldownImage : MonoBehaviour
 {
     // Need images for the skills to show the Player which skill is on cooldown
-    
+
     // This script will be responsible for updating the cooldown images and texts for the special attack and healing skills
     // Might do the Fireball maybe.
     // Dash will have a cooldown but I need it in here first. Dash is going to have a circle that fills up and not a square.
@@ -15,12 +15,16 @@ public class SkillCooldownImage : MonoBehaviour
     public TextMeshProUGUI[] cooldownTexts;
     private PlayerController playerController;
 
+    public GameObject healingObject;
+    public GameObject waveObject;
+
     void Start()
     {
         if (playerController == null)
         {
             Debug.Log("PlayerController not found.");
         }
+
         playerController = FindObjectOfType<PlayerController>();
     }
 
@@ -34,14 +38,25 @@ public class SkillCooldownImage : MonoBehaviour
                 return;
             }
         }
-        
+
         if (playerController != null)
         {
-            float[] cooldownTimes = { playerController.nextSpecialAttackTime, playerController.nextHealingTime, playerController.nextDashTime };
-            float[] cooldownDurations = { playerController.specialAttackCooldown, playerController.healingCooldown, playerController.dashCooldown };
-            bool[] skillUnlocked = { playerController.isSpecialAttackUnlocked, playerController.isHealingUnlocked, playerController.isDashUnlocked };
+            float[] cooldownTimes =
+            {
+                playerController.nextSpecialAttackTime, playerController.nextHealingTime, playerController.nextDashTime
+            };
+            float[] cooldownDurations =
+            {
+                playerController.specialAttackCooldown, playerController.healingCooldown, playerController.dashCooldown
+            };
+            bool[] skillUnlocked =
+            {
+                playerController.isSpecialAttackUnlocked, playerController.isHealingUnlocked, true
+            }; // Dash is always unlocked
 
-            for (int i = 0; i < cooldownImages.Length; i++)
+            int minLength = Mathf.Min(cooldownImages.Length, cooldownTexts.Length, skillUnlocked.Length);
+
+            for (int i = 0; i < minLength; i++)
             {
                 if (skillUnlocked[i])
                 {
@@ -52,17 +67,31 @@ public class SkillCooldownImage : MonoBehaviour
                     cooldownImages[i].fillAmount = Mathf.Clamp01(cooldownTime / cooldownDuration);
                     cooldownImages[i].gameObject.SetActive(true);
                     cooldownTexts[i].gameObject.SetActive(true);
+
+                    if (i == 1) // Assuming index 1 is for healing skill
+                    {
+                        healingObject.SetActive(true);
+                    }
+                    else if (i == 0) // Assuming index 0 is for wave skill
+                    {
+                        waveObject.SetActive(true);
+                    }
                 }
                 else
                 {
                     cooldownImages[i].gameObject.SetActive(false);
                     cooldownTexts[i].gameObject.SetActive(false);
+
+                    if (i == 1) // Assuming index 1 is for healing skill
+                    {
+                        healingObject.SetActive(false);
+                    }
+                    else if (i == 0) // Assuming index 0 is for wave skill
+                    {
+                        waveObject.SetActive(false);
+                    }
                 }
             }
         }
     }
-    
-    /* BUGS:
-     Cooldown counts to the negative numbers - FIXED
-     */
 }
